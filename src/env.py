3eb51552156b460039f4dbceb454b9de31a7b70c
Chris Cox <chris.cox@westfall.io>
2024-12-03 16:40:19 -0500

    :bug: Adding pyproject.toml

---
+++ src/env.py
@@ -0,0 +1,19 @@
+import os
+VOLDEF = "/mnt/vol"
+VOLUME = os.environ.get("VOLUME",VOLDEF)
+WINDRUNNERHOST = os.environ.get(
+    "WINDRUNNERHOST",
+    "http://windrunner-webhook-eventsource-svc.argo-events:12000/windrunner"
+)
+WINDSTORMAPICLIENT = os.environ.get("WINDSTORMAPICLIENT","")
+WINDSTORMAPISECRET = os.environ.get("WINDSTORMAPISECRET","")
+
+WINDSTORMAPIHOST = os.environ.get(
+    "WINDSTORMAPIHOST",
+    "http://windstorm-api-service.windstorm:8000/"
+)
+
+## MINIO VALUES
+MINIOHOST = os.environ.get("MINIOHOST","storage-minio.artifacts:9000")
+MINIOUSER = os.environ.get("MINIOUSER","CcgP5DINKOfemEXcjYyL")
+MINIOTOKEN = os.environ.get("MINIOUSER","YS62HYwroWYozFGoWyeZjYsmGwFLEULu047lquE6")
